cbf565fd9e53b1fc4255fad06520a44e5af981d0
yurii.d <relantii@mail.ru>
2023-03-23 17:50:32 +0300

    add new game gcd in new filegcd.js

---
+++ bin/games/brain-gcd.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import { getRandomNumber } from "../getRandomNumber.js";
+import { runGame } from "../../src/index.js";
+
+const discription = 'Find the greatest common divisor of given numbers.';
+
+const resultExpression = (a, b) => {
+    let c;
+    while (b !== 0) {
+        c = b;
+        b = a % b;
+        a = c;
+    }
+    return a;
+};
+
+const gameFunction = () => {
+    const num1 = getRandomNumber(1, 20);
+    const num2 = getRandomNumber(1, 20);
+    const question = `${num1} ${num2}`;
+    const rigthAnswer = String(resultExpression(num1, num2));
+    return [question, rigthAnswer];
+};
+
+const startGame = () => {
+    runGame(discription, gameFunction);
+}
+
+startGame();
