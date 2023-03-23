71126ca5fd6a7154d88e9a64e8a00b6cc797437e
yurii.d <relantii@mail.ru>
2023-03-23 17:52:28 +0300

    add change brain-calc.js

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
-import readlineSync from "readline-sync";
 import { getRandomNumber } from "../getRandomNumber.js";
+import { runGame } from "../../src/index.js";
 
-var name = readlineSync.question("May I have your name? ");
-const greet = `${"Hello, "}${name}${"!"}`
-console.log(greet);
-const question = `${'What is the result of the expression?'}`;
-console.log(question);
+const discription = 'What is the result of the expression?';
 
 
 const resultExpression = (num1, num2, getRandomSign) => {
@@ -27,28 +23,18 @@ const resultExpression = (num1, num2, getRandomSign) => {
     return expression;
 };
 
-const funcGame = () => {
-    let count = 0;
-    while (count < 3) {
-        const operator = ['+', '-', '*'];
-        const getRandomSign = operator[Math.floor(Math.random() * operator.length)];
-        const num1 = getRandomNumber(1, 10);
-        const num2 = getRandomNumber(1, 10);
-        const question = `${num1} ${getRandomSign} ${num2}`;
-        const answer = String(resultExpression(num1, num2, getRandomSign));
-        const questionUser = `${'Question: '}${question}`;
-        console.log(questionUser);
-        const answerUser = readlineSync.question('Your answer: ');
-        if (answer !== answerUser) {
-            console.log(`${answerUser}${' is wrong answer ;(. Correct answer was '}${answer}${'.'}\n${'Let\'s try again, '}${name}`);
-            break;
-        } else {
-            console.log('Correct!');
-            count += 1;
-        }
-    } if (count === 3) {
-        console.log(`${'Congratulations '}${name}${'!'}`)
-    }
+const gameFunction = () => {
+    const operator = ['+', '-', '*'];
+    const getRandomSign = operator[Math.floor(Math.random() * operator.length)];
+    const num1 = getRandomNumber(1, 10);
+    const num2 = getRandomNumber(1, 10);
+    const question = `${num1} ${getRandomSign} ${num2}`;
+    const rigthAnswer = String(resultExpression(num1, num2, getRandomSign));
+    return [question, rigthAnswer]
 };
 
-funcGame();
+const startGame = () => {
+    runGame(discription, gameFunction);
+};
+
+startGame();
