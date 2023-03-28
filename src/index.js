6e7169ccf49ea726b7146e19c1c10f4c9d0dafe5
yurii.d <relantii@mail.ru>
2023-03-28 21:07:46 +0300

    fix content

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from "readline-sync";
 const runGame = (discritption, gameFunction) => {
     console.log('Welcome to the Brain Games!');
     const name = readlineSync.question('May I have you name? ');
-    console.log(`${'Hello, '}${name}`);
+    console.log(`${'Hello, '}${name}${'!'}`);
     console.log(discritption);
 
     const count = 3;
